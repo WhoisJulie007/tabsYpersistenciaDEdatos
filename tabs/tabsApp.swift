5b41d69aebc8fb69460e22d99a0599d8f9988abc
Julia <maydeli.castan@iest.edu.mx>
2025-10-31 17:52:39 -0600

    Initial Commit

---
+++ tabs/tabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabsApp.swift
+//  tabs
+//
+//  Created by WIN603 on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct tabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
