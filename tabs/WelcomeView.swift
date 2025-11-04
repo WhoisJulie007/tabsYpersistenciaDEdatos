dbee5640b4a5bfff66e7300010a545435f86b20c
eunicecompean <eunice.chavez@iest.edu.mx>
2025-11-03 18:13:40 -0600

    Nuevos cambios con la Welcome View

--- tabs/WelcomeView.swift
+++ tabs/WelcomeView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @AppStorage("myName") var savedName: String = ""
+    @AppStorage("myAge") var savedAge: Int = 0
+    
+    @State var name: String = ""
+    @State var ageText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("Dime tu nombre:", text: $name)
+                .padding()
+            
+            TextField("Dime tu edad:", text: $ageText)
+                .padding()
+                
+            Button(action: {
+                savedName = self.name
+                
+                if let edad = Int(self.ageText){
+                    savedAge = edad
+                }
+                
+            }, label: {
+                Text("Guardar").padding().background(Color.green)
+            }).padding()
+            
+            
+            Text("Bienvenido \(savedName) con edad \(savedAge)")
+        }
     }
 }
 
-#Preview {
-    WelcomeView()
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
 }
+
